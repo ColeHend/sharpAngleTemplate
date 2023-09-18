872db9f4f1efa2506c0bd30b5f501b68b49f28e5
ColeHend <darkgale93@gmail.com>
2023-09-17 18:34:56 -0600

    user stuff

---
+++ models/UserReq.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sharpAngleTemplate.models
+{
+    public class UserRegisterReq
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string MoreData { get; set; }
+    }
+    public class UserLoginReq
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+    public class UserGetReq
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+    }
+    public class UserUpdateReq
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string MoreData { get; set; }
+    }
+}
--- models/entities/UserEntity.cs
+++ models/entities/UserEntity.cs
@@ -2,11 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
 namespace sharpAngleTemplate.models.entities
 {
+    [Index(nameof(Username), IsUnique = true)]
     public class User
     {
+        [Key]
         public int Id { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
